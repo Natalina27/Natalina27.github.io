b232f0e68b7923284ea412a4ce360c8815438555
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-10 20:39:20 +0200

    querySelector()

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-// let input = document.querySelector('search');
-//  console.log(input);
-// let output = document.querySelector('search');
-// console.log(output);
-//
-// input.addEventListener('change', function () {
-//     output.innerHTML = input.value;
-// })
-const sum = require('./utils');
-console.log('Hello world!');
 
-console.log(sum(2,4));
+// const sum = require('./utils');
+// console.log('Hello world!');
+//
+// console.log(sum(2,4));
+
+const searchInput = document.querySelector('#wrapper input .name');
+console.log(searchInput);
