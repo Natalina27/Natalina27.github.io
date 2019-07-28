e87f2da91dfba9ba9d2cd3b35a33fccd56f4bc63
Natalya <natalyamyunster@yahoo.com>
2019-07-28 12:37:25 +0200

    add gitignore

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@
 
 let input = document.querySelector('#wrapper header form input');
 console.log(input);
+
+let input1 = document.querySelector('#heading');
+console.log(input1);
+
+input1.innerHTML += '<p>rrrr</p>';
+
+
+// const list = document.querySelector('#wrapper header nav ul');
+// const searchBar = document.forms['search'].querySelector('input');
+// console.log(searchBar);
+// searchBar.addEventListener('keyup', function (e) {
+//     const term = e.target.value.toLowerCase();
+//     const books = list.getElementsByTagName('li');
+//     Array.from(books).forEach(function (book) {
+//         const title = book.firstElementChild.textContent;
+//         if(title.toLowerCase().indexOf(term) !== -1){
+//             book.style.display = 'block';
+//         }else{
+//             book.style.display = 'none';
+//         }
+//     })
+// })
